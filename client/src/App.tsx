15f7e15b7f5532edbc799d1949ceeb0012cae5c5
ArnoldRamodike <riccoarnold23@gmail.com>
2024-09-05 23:47:53 +0200

    Code updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ export const BASE_URL =
   import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "/api";
 function App() {
   return (
-    <Stack h="100vh" w="100vh">
+    <Stack h="100vh" width="max-content">
       <Navbar />
       <Container>
         <TodoForm />
